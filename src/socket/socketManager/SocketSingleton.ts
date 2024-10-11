2809b5da8bb7338e06b1d119d3c606494cc5e0a1
Ashwani Kumar <ashwanikr160@gmail.com>
2024-10-11 08:49:45 +0530

    updated socket io link

--- src/socket/socketManager/SocketSingleton.ts
+++ src/socket/socketManager/SocketSingleton.ts
@@ -1,15 +1,15 @@
 import { io } from "socket.io-client";
 import { SocketManager } from "./SocketManager";
 
-const SERVER_URL = String(process.env.NEXT_PUBLIC_SERVER_URL);
+const SOCKET_URL = String(process.env.NEXT_PUBLIC_SOCKET_URL);
 
-console.log(SERVER_URL);
+console.log(SOCKET_URL);
 class SocketSingleton {
   private static instance: SocketManager | null = null;
 
   public static getInstance(): SocketManager {
     if (!SocketSingleton.instance) {
-      const socket = io(SERVER_URL, {
+      const socket = io(SOCKET_URL, {
         autoConnect: false,
         withCredentials: true,
         //path: "/api/socket.io", // Changed this line
